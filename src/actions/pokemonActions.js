ee97aefe04bb6c8101c932bdeacb7574f6f5ff41
kleyu <jackob.naskreski@gmail.com>
2018-08-29 22:26:15 +0200

    add local state to pagination, refactor getPokemon action to handle page parameter

--- src/actions/pokemonActions.js
+++ src/actions/pokemonActions.js
@@ -1,7 +1,9 @@
 import { GET_POKEMON } from "./types";
 
-export const getPokemon = () => async dispatch => {
-	const res = await fetch("http://localhost:3004/pokemon?_page=1&_limit=20");
+export const getPokemon = page => async dispatch => {
+	const res = await fetch(
+		`http://localhost:3004/pokemon?_page=${page}&_limit=20`
+	);
 	const json = await res.json();
 	dispatch({
 		type: GET_POKEMON,
--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -2,20 +2,29 @@ import React from "react";
 
 export default ({ pokemon }) => {
 	let typeColors = {
-		Grass: "green",
-		Poison: "purple",
-		Fire: "red",
-		Flying: "aqua",
-		Water: "blue",
-		Bug: "Chartreuse",
-		Normal: "grey"
+		Grass: "#7c5",
+		Poison: "#a59",
+		Fire: "#f42",
+		Flying: "#89f",
+		Water: "#39f",
+		Bug: "#ab2",
+		Normal: "#aa9",
+		Psychic: "#f59",
+		Electric: "#fc3",
+		Ground: "#db5",
+		Fighting: "#b54",
+		Rock: "#ba6",
+		Ice: "#6cf",
+		Ghost: "#66b"
 	};
 
 	let style = {
 		color: "white",
 		padding: "1px 8px 4px 8px",
 		borderRadius: "10px",
-		margin: "2px"
+		margin: "2px",
+		border: "1px solid rgba(0,0,0,0.2)",
+		textShadow: "1px 1px 2px rgba(0,0,0,0.7)"
 	};
 
 	return (
