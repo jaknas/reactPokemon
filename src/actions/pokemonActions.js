079fbff6ec6e02f30e2ae816f989f809f65fa6d1
kleyu <jackob.naskreski@gmail.com>
2018-08-30 01:02:08 +0200

    add more info button which opens up a modal with all the info

--- src/actions/pokemonActions.js
+++ src/actions/pokemonActions.js
@@ -1,12 +1,16 @@
 import { GET_POKEMON } from "./types";
 
 export const getPokemon = page => async dispatch => {
-	const res = await fetch(
-		`http://localhost:3004/pokemon?_page=${page}&_limit=20`
-	);
-	const json = await res.json();
-	dispatch({
-		type: GET_POKEMON,
-		payload: json
-	});
+	try {
+		const res = await fetch(
+			`http://localhost:3004/pokemon?_page=${page}&_limit=20`
+		);
+		const json = await res.json();
+		dispatch({
+			type: GET_POKEMON,
+			payload: json
+		});
+	} catch (error) {
+		console.log("Error when fetching data");
+	}
 };
